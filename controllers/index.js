8c38e9ee898b26d7ee6571e1135a414e1e6083cb
Sebastian Czajkowski <czaj.sebastian@gmail.com>
2020-12-06 18:51:35 +0100

    Upgrade hotel randomization

--- controllers/index.js
+++ controllers/index.js
@@ -2,15 +2,17 @@ const Hotel = require("../models/hotel")
 
 exports.showIndex = async (req, res) => {
     await Hotel.find({}, (err, result) => {
-        if (result != null) {
-            hotels = []
-            let first = Math.floor(Math.random() * (result.length))
-            let second;
-            hotels.push(result[first])
-            do {
-                second = Math.floor(Math.random() * (result.length))
-            } while (first == second);
-            hotels.push(result[second])
+        if (result.length > 0) {
+            let hotels = []
+            let indexes = []
+            let condition = result.length < 4 ? result.length : 4
+            for (let i = 0; i < condition; i++){
+                do {
+                    var tmp = Math.floor(Math.random() * (result.length))
+                } while (indexes.includes(tmp, 0));
+                indexes.push(tmp)
+                hotels.push(result[indexes[i]])
+            }
             return res.render('index.ejs', { session: req.session, hotels: hotels })
         }
         return res.render('index.ejs', { session: req.session })
